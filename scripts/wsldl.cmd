447c043297be89429bbcfdf5fb6cbabe6c88b5d9
beni69 <73666440+beni69@users.noreply.github.com>
2021-10-05 22:03:24 +0200

    epic custom installer (maybe it works)

---
+++ scripts/wsldl.cmd
@@ -0,0 +1,16 @@
+:: generate a wsldl exe with an epic icon
+cd build
+
+curl.exe -o rcedit.exe "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe"
+
+mkdir wsldl
+cd wsldl
+
+curl.exe -O "https://github.com/yuk7/wsldl/releases/latest/download/wsldl.exe"
+
+../rcedit.exe wsldl.exe --set-icon ../../etc/karesz.ico
+
+copy ..\kareszwsl.tar.gz .\kareszwsl.tar.gz
+cd ..
+
+powershell Compress-Archive wsldl/* wsldl.zip
