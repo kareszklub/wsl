b630e4e095aa0b5b9838b85d929819e45f20c702
beni69 <73666440+beni69@users.noreply.github.com>
2021-10-06 22:13:20 +0200

    epic home dir

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -13,20 +13,24 @@ COUNTRY_CODE=$(curl -sL ipinfo.io/country)
 
 # create default user
 # (sudo will need a password later, this is userful for the installer)
+echo "creating user"
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/wheel
 chmod 440 /etc/sudoers.d/wheel
 useradd -mG wheel -s /bin/bash diak -p '$6$nI/Ya5wm4s9xzbWL$3bmSK4Y.vqFnxDQCof.sMlDmnNHzh/7pUGUoJO9RUu4l9ZA.De/MebzGCDBP9iiJ0y1NHYfORX2D.APIbjnlx1'
 
+# copy base home directory
+echo "copying base home directory"
+sudo -u diak cp -rvL "$SCRIPT_DIR/../home/*" "/home/diak/"
 # pacman setup
 pacman-key --init && pacman-key --populate
 
+# install some packages
+echo "installing packages"
 # generate mirrorlist
-pacman -Sy --noconfirm reflector
+pacman -Sy --noconfirm --needed reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 echo "generating mirrorlist in $COUNTRY_CODE"
 reflector -c $COUNTRY_CODE -p https --sort rate --save /etc/pacman.d/mirrorlist
-
-# install some packages
 pacman -Syu --noconfirm --needed \
     autoconf \
     automake \
@@ -64,11 +68,16 @@ pacman -Syu --noconfirm --needed \
     python \
     python2 \
     python-pip \
-    python2-pip
+    python2-pip \
+    bind-tools \
+    tree
 
 # run user script
+echo "running user script"
 sudo -u diak bash "$SCRIPT_DIR/userspace.sh"
 
 # sudo requires password
 echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel
 chmod 440 /etc/sudoers.d/wheel
+
+echo "post-install script completed"
