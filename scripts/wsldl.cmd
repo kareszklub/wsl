4547787bd8ba2405b1b44e438c9d2ee9b0207fc0
beni69 <73666440+beni69@users.noreply.github.com>
2021-10-06 17:04:38 +0200

    fix errors in wsldl script

--- scripts/wsldl.cmd
+++ scripts/wsldl.cmd
@@ -1,16 +1,12 @@
 :: generate a wsldl exe with an epic icon
 cd build
-
-curl.exe -o rcedit.exe "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe"
-
 mkdir wsldl
-cd wsldl
 
-curl.exe -O "https://github.com/yuk7/wsldl/releases/latest/download/wsldl.exe"
+curl.exe -Lo rcedit.exe "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe"
+curl.exe -Lo wsldl/kareszwsl.exe "https://github.com/yuk7/wsldl/releases/latest/download/wsldl.exe"
 
-..\rcedit.exe wsldl.exe --set-icon ..\..\etc\karesz.ico
+rcedit.exe wsldl\kareszwsl.exe --set-icon ..\etc\karesz.ico
 
-copy ..\kareszwsl.tar.gz .\rootfs.tar.gz
-cd ..
+copy kareszwsl.tar.gz wsldl\rootfs.tar.gz
 
 powershell Compress-Archive wsldl/* wsldl.zip
